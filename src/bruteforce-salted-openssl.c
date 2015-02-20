420a9b3e77ca80d119ae34b4d2d36431da690fab
Simon Wood <simon@mungewell.org>
2015-02-19 20:59:19 -0700

    Binary Mode Passphrases
    Add '-B <string>' parameter to use a binary passphrase and output candidate
    passphrasee to file '<string>-<count>'.
    
    This is intended for brute-forcing files which have been encrypted like
    --
    $ openssl rand 32 -out keyfile
    $ openssl aes-256-cbc -e -in plaintext -out plaintext.enc -pass file:keyfile -salt
    --

--- src/bruteforce-salted-openssl.c
+++ src/bruteforce-salted-openssl.c
@@ -31,12 +31,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "version.h"
 
 unsigned char *default_charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-unsigned char *charset = NULL, *data = NULL, salt[8], *prefix = NULL, *suffix = NULL;
+
+                                 /* 0x00 is not included as passphrases are null terminated */
+unsigned char *binary_charset =      "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
+                                 "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
+                                 "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2A\x2B\x2C\x2D\x2E\x2F"
+                                 "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3A\x3B\x3C\x3D\x3E\x3F"
+                                 "\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F"
+                                 "\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x5B\x5C\x5D\x5E\x5F"
+                                 "\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F"
+                                 "\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x7B\x7C\x7D\x7E\x7F"
+                                 "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F"
+                                 "\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F"
+                                 "\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF"
+                                 "\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF"
+                                 "\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF"
+                                 "\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF"
+                                 "\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF"
+                                 "\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF";
+
+unsigned char *charset = NULL, *data = NULL, salt[8], *prefix = NULL, *suffix = NULL, *binary = NULL;
 unsigned int charset_len = 62, data_len = 0, min_len = 1, max_len = 8, prefix_len = 0, suffix_len = 0;
 const EVP_CIPHER *cipher = NULL;
 const EVP_MD *digest = NULL;
 pthread_mutex_t found_password_lock;
 char stop = 0, only_one_password = 0;
+int solution = 0;
 
 
 /*
@@ -123,7 +143,22 @@ void * decryption_func(void *arg)
                 {
                   /* We have a positive result */
                   pthread_mutex_lock(&found_password_lock);
-                  printf("Password candidate: %s\n", password);
+                  if (binary == NULL)
+                    printf("Password candidate: %s\n", password);
+                  else {
+                    int fd;
+                    char outfile[128];
+
+                    sprintf(outfile, "%s-%d", binary, solution++);
+                    fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+                    if (fd == -1)
+                      perror("open file");
+                    else {
+                      printf("Password candidate saved to file: %s \n", outfile);
+                      write(fd, password, password_len);
+                      close(fd);
+                    }
+                  }
                   if(only_one_password)
                     stop = 1;
                   pthread_mutex_unlock(&found_password_lock);
@@ -244,6 +279,7 @@ void usage(char *progname)
   fprintf(stderr, "                           VWXYZabcdefghijklmnopqrstuvwxyz\"\n");
   fprintf(stderr, "  -t <n>       Number of threads to use.\n");
   fprintf(stderr, "                 default: 1\n");
+  fprintf(stderr, "  -B <string>  Search using binary passphrase, write candidates to file <string>.\n");
   fprintf(stderr, "\n");
 }
 
@@ -269,7 +305,7 @@ int main(int argc, char **argv)
 
   /* Get options and parameters */
   opterr = 0;
-  while((c = getopt(argc, argv, "1ab:c:d:e:hl:m:s:t:")) != -1)
+  while((c = getopt(argc, argv, "1ab:c:d:e:hl:m:s:t:B:")) != -1)
     switch(c)
       {
       case '1':
@@ -330,6 +366,10 @@ int main(int argc, char **argv)
           nb_threads = 1;
         break;
 
+      case 'B':
+        binary = optarg;
+        break;
+
       default:
         usage(argv[0]);
         switch(optopt)
@@ -342,6 +382,7 @@ int main(int argc, char **argv)
           case 'm':
           case 's':
           case 't':
+          case 'B':
             fprintf(stderr, "Error: missing argument for option: '-%c'.\n\n", optopt);
             break;
 
@@ -374,7 +415,10 @@ int main(int argc, char **argv)
     suffix = "";
   suffix_len = strlen(suffix);
   if(charset == NULL)
-    charset = default_charset;
+    if(binary == NULL)
+      charset = default_charset;
+    else
+      charset = binary_charset;
   charset_len = strlen(charset);
   if(charset_len == 0)
     {
